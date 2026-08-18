68717cb9199e438ba8a08a390d8d33500e9d84af
Poojaaa <jhapooja0099@gmail.com>
2026-08-18 23:28:36 +0530

    Add EmptyState component for no courses available

---
+++ EmptyState.tsx
@@ -0,0 +1,13 @@
+export function EmptyState() {
+    return (
+        <div
+            style={{
+                textAlign: "center",
+                padding: 40,
+                color: "#666",
+            }}
+        >
+            <p>No courses are available right now.</p>
+        </div>
+    )
+}
